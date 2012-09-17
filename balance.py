2d2e1436e45cb46ff6cf9741def77faad6cdc8a4
nikuyoshi <nikuyoshi@gmail.com>
2012-09-18 00:25:41 +0900

    first commit

---
+++ balance.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8 
+
+import sys
+import fileinput
+import codecs
+from datetime import date
+
+argvs = sys.argv[1:]
+argc = len(argvs)
+# print argvs
+# print argc
+# print argvs[0]
+# print argvs[1]
+# print argvs[2]
+
+for i in range(len(argvs)):
+    argvs[i] = unicode(argvs[i], 'utf-8')
+# print argvs[2]
+
+def add():
+    print
+    if (argc != 3) :
+        print 'Error: wrong number of arguments: add? requires 3'
+    else:
+        today = str(date.today())
+        f = codecs.open('cashbook.txt','a+','utf-8')
+        f.write(today+'\t'+ argvs[1] +'\t'+argvs[2]+'\n')
+        f.close()
+
+def summary():
+    allLines = open('cashbook.txt').read() 
+    print allLines
+    #f = open('cashbook.txt', 'r')
+    # for line in f:
+    #     # test = str(line)
+    #     # print line
+    #     # print test[0]
+    #     # print '---'
+    #     itemList = line[:-1].split('\t')
+    #     itemList[2] =  
+    #     print itemList
+    #f.close() 
+
+if (argvs[0] == 'add'):
+    add()
+elif (argvs[0] == 'summary'):
+    summary()
