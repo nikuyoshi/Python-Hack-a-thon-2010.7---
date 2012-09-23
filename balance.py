0cf348f6d80628f9bca6eba088b555fb13e15715
nikuyoshi <nikuyoshi@gmail.com>
2012-09-23 22:28:03 +0900

    課題2終了

--- balance.py
+++ balance.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# coding: utf-8 
+# -*- coding: utf-8  -*-
 
 import sys
 import fileinput
@@ -8,41 +8,42 @@ from datetime import date
 
 argvs = sys.argv[1:]
 argc = len(argvs)
-# print argvs
-# print argc
-# print argvs[0]
-# print argvs[1]
-# print argvs[2]
+if (argc == 0) :
+    print 'Error: wrong number of arguments: 1 and over'
+else:
+    for i in range(len(argvs)):
+        argvs[i] = unicode(argvs[i], 'utf-8')
 
-for i in range(len(argvs)):
-    argvs[i] = unicode(argvs[i], 'utf-8')
-# print argvs[2]
+    def add():
+        print
+        if (argc != 3) :
+            print 'Error: wrong number of arguments: add? requires 3'
+        else:
+            today = str(date.today())
+            f = codecs.open('cashbook.txt','a+','utf-8')
+            f.write(today+'\t'+ argvs[1] +'\t'+argvs[2]+'\n')
+            f.close()
 
-def add():
-    print
-    if (argc != 3) :
-        print 'Error: wrong number of arguments: add? requires 3'
-    else:
-        today = str(date.today())
-        f = codecs.open('cashbook.txt','a+','utf-8')
-        f.write(today+'\t'+ argvs[1] +'\t'+argvs[2]+'\n')
-        f.close()
+    def summary():
+        f = open('cashbook.txt', 'r')
+        ################################################
+        # itemList = []
+        # for line in f:
+        #     itemList.append( line[:-1].split('\t') )
+        # print itemList
+        ################################################
+        # ↑をリスト内包表記に書き換え、タブ・改行を無くしてリストに入れる
+        itemList = [line[:-1].split('\t') for line in f]
+        date = {} 
+        for x in range(len(itemList)):
+            if itemList[x][0] in date:
+                 date[itemList[x][0]] += int(itemList[x][1])
+            else :
+                 date[itemList[x][0]] = int(itemList[x][1])
+        for (i, j) in date.items():
+            print '%s \t %d' %(i, j)
 
-def summary():
-    allLines = open('cashbook.txt').read() 
-    print allLines
-    #f = open('cashbook.txt', 'r')
-    # for line in f:
-    #     # test = str(line)
-    #     # print line
-    #     # print test[0]
-    #     # print '---'
-    #     itemList = line[:-1].split('\t')
-    #     itemList[2] =  
-    #     print itemList
-    #f.close() 
-
-if (argvs[0] == 'add'):
-    add()
-elif (argvs[0] == 'summary'):
-    summary()
+    if (argvs[0] == 'add'):
+        add()
+    elif (argvs[0] == 'summary'):
+        summary()
